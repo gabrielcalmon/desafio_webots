40bc76a9e9dc7c97cb0181702ba80caed56a4dbd
Vitor Mendes <vitorsmendscod@gmail.com>
2021-05-14 18:07:05 -0300

    //

---
+++ controllers/desafio-webots/build/release/desafio-webots.d
@@ -0,0 +1,8 @@
+build/release/desafio-webots.o: desafio-webots.c \
+ /snap/webots/18/usr/share/webots/include/controller/c/webots/distance_sensor.h \
+ /snap/webots/18/usr/share/webots/include/controller/c/webots/types.h \
+ /snap/webots/18/usr/share/webots/include/controller/c/webots/led.h \
+ /snap/webots/18/usr/share/webots/include/controller/c/webots/light_sensor.h \
+ /snap/webots/18/usr/share/webots/include/controller/c/webots/motor.h \
+ /snap/webots/18/usr/share/webots/include/controller/c/webots/robot.h \
+ /snap/webots/18/usr/share/webots/include/controller/c/webots/nodes.h
